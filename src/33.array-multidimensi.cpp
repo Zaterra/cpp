0399c9b9e5b3abe98a49c3f4df028d80fdc3b1f0
Zaterra <zaterrazy@gmail.com>
2024-04-03 05:25:17 +0700

    modified:   src/33.array-multidimensi.cpp 	new file:   src/34.array-multidimensi-std.cpp 	renamed:    src/34.vector.cpp -> src/35.vector.cpp
    +menyelesaikan 33.array-multidimensi.cpp
    +menambah file 34.array-multidimensi-std.cpp (berisi tentang cara membuat array-md dengan standar lib array)
    +mengubah nama 34.vector.cpp menjadi 35.vector.cpp

--- src/33.array-multidimensi.cpp
+++ src/33.array-multidimensi.cpp
@@ -2,10 +2,31 @@
 
 using std::cout;
 using std::cin;
+using std::endl;
+
+void printArray(int *pointerArray, int baris, int kolom){
+    int index = 0;
+
+    for(int i = 0; i<baris; i++){
+        cout << "[ ";
+        for(int j = 0; j<kolom; j++){
+            cout << *(pointerArray + index)  << " ";
+            index++;
+        }
+        cout << "]" << endl;
+    }
+}
 
 int main(){
     // array multidimensi
-    int arrayMD[2][2] = {1,2,3,4};
+    // arrray 2d
+    const int baris = 2;
+    const int kolom = 2;
+    int arrayMD[baris][kolom] = {1,2,
+                                 3,4};
+    cout << arrayMD[0][0] << " " << arrayMD[0][1] << endl;
+    cout << arrayMD[1][0] << " " << arrayMD[1][1] << endl;
 
+    printArray(*arrayMD, 2, 2);
     return 0;
 }
